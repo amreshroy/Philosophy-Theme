d856a845c8862557391ca9cb1616ee78cf859a4f
AmreshRoy <amoreshray@gmail.com>
2021-02-05 22:10:14 +0600

    Prev/Next post section added

--- single.php
+++ single.php
@@ -71,15 +71,29 @@ get_header();
                 <div class="s-content__pagenav">
                     <div class="s-content__nav">
                         <div class="s-content__prev">
-                            <a href="#0" rel="prev">
-                                <span>Previous Post</span>
-                                Tips on Minimalist Design 
+                            <?php
+                            $philosophy_prev_post = get_previous_post();
+                            if($philosophy_prev_post): ?>
+                            <a href="<?php echo get_the_permalink($philosophy_prev_post); ?>" rel="prev">
+                                <span><?php _e("Previous Post", "philosophy"); ?></span>
+                                <?php echo get_the_title($philosophy_prev_post); ?>
                             </a>
+                            <?php
+                            endif;
+                            ?>
                         </div>
                         <div class="s-content__next">
                             <a href="#0" rel="next">
-                                <span>Next Post</span>
-                                Less Is More 
+                            <?php
+                            $philosophy_next_post = get_next_post();
+                            if($philosophy_next_post): ?>
+                            <a href="<?php echo get_the_permalink($philosophy_next_post); ?>" rel="next">
+                                <span><?php _e("Next Post", "philosophy"); ?></span>
+                                <?php echo get_the_title($philosophy_next_post); ?>
+                            </a>
+                            <?php
+                            endif;
+                            ?> 
                             </a>
                         </div>
                     </div>
