2f2d4d66f4c5a34d5ce7516222332c45ffa41c39
AmreshRoy <amoreshray@gmail.com>
2021-02-09 23:17:58 +0600

    Before footer tags section

--- footer.php
+++ footer.php
@@ -28,6 +28,7 @@
                 </article>
             <?php
                 }
+                wp_reset_query();
             ?>
             
         </div> <!-- end popular_posts -->
@@ -63,17 +64,9 @@
         <h3>Tags</h3>
 
         <div class="tagcloud">
-            <a href="#0">Salad</a>
-            <a href="#0">Recipe</a>
-            <a href="#0">Places</a>
-            <a href="#0">Tips</a>
-            <a href="#0">Friends</a>
-            <a href="#0">Travel</a>
-            <a href="#0">Exercise</a>
-            <a href="#0">Reading</a>
-            <a href="#0">Running</a>
-            <a href="#0">Self-Help</a>
-            <a href="#0">Vacation</a>
+
+            <?php the_tags(" "," "," "); ?>
+
         </div> <!-- end tagcloud -->
     </div> <!-- end tags -->
 </div> <!-- end tags-wrap -->
