610f0782787f8db18e3e6aabf8adaa5352113db4
AmreshRoy <amoreshray@gmail.com>
2021-03-20 18:29:39 +0600

    Archive page search

--- functions.php
+++ functions.php
@@ -148,12 +148,21 @@ function philosophy_search_form(){
     $homedir = home_url("/");
     $label   = __("Search for: ", "philosophy");
     $button_label = __("Search", "philosophy");
+    $post_type    = <<<PT
+    <input type="hidden" name="post_type" value="post">
+    PT;
+    if(is_post_type_archive('movie')){
+        $post_type    = <<<PT
+        <input type="hidden" name="post_type" value="movie">
+        PT;
+    }
     $newform = <<<FORM
     <form role="search" method="get" class="header__search-form" action="{$homedir}">
         <label>
             <span class="hide-content">{$label}</span>
             <input type="search" class="search-field" placeholder="Type Keywords" value="" name="s" title="{$label}" autocomplete="off">
         </label>
+        {$post_type}
         <input type="submit" class="search-submit" value="{$button_label}">
     </form>
     FORM;
